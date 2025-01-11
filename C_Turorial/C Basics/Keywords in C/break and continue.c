9fd67f767a15b60f9d6286076063b519a032cba7
MONU KUSHAWAHA <monukushawaha7@gmail.com>
2025-01-12 00:53:19 +0530

    C program to show use of break and continue

---
+++ C_Turorial/C Basics/Keywords in C/break and continue.c
@@ -0,0 +1,20 @@
+// C program to show use of break and continue
+#include <stdio.h>
+
+// Driver code
+int main()
+{
+  for (int i = 1; i <= 10; i++) 
+  {
+    if (i == 2) 
+    {
+      continue;
+    }
+    if (i == 6) 
+    {
+      break;
+    }
+    printf("%d ", i);
+  }
+  return 0;
+}

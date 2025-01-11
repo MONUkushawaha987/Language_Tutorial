619e4f96d7930eb58ee35835a1bd26fe6202dd60
MONU KUSHAWAHA <monukushawaha7@gmail.com>
2025-01-12 01:12:16 +0530

    C program to demonsstrate while keyword

---
+++ C_Turorial/C Basics/Keywords in C/while.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int i = 0;
+
+    // While loop that execute till i is less than 3
+    while (i < 3) {
+        printf("Hi\n");
+        i++;
+    }
+  
+    return 0;
+}

7f3e4d01a7283e518c2601c642c6ad7d8afa336e
MONU KUSHAWAHA <monukushawaha7@gmail.com>
2025-01-12 01:06:06 +0530

    C program to demonstrate int keyword

---
+++ C_Turorial/C Basics/Keywords in C/int.c
@@ -0,0 +1,16 @@
+// C program to demonstrate
+// int keyword
+#include <stdio.h>
+
+void sum() {
+    int a = 10, b = 20;
+    int sum;
+    sum = a + b;
+    printf("%d", sum);
+}
+
+// Driver code
+int main() {
+    sum();
+    return 0;
+}

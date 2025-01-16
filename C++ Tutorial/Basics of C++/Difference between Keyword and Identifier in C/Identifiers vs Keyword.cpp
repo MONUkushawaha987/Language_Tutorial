170324a09c75ab54b4e6e103f6cd65cb3f71b38e
MONU KUSHAWAHA <monukushawaha7@gmail.com>
2025-01-16 12:43:56 +0530

    different purpose of the keywords and identifers in C++

---
+++ C++ Tutorial/Basics of C++/Difference between Keyword and Identifier in C/Identifiers vs Keyword.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+// 'main' is also an identifier
+// although it is predefined
+int main() {
+  
+    // Example of a keyword: int
+      // Here, age is identifier
+    int age = 25;
+
+    printf("Age: %d\n", age);
+
+    // return is a keyword that exits function
+    return 0; 
+}

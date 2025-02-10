98b231b99f82397d60310aee3cc1d289562d1435
MONU KUSHAWAHA <monukushawaha7@gmail.com>
2025-02-10 11:19:06 +0530

    operators tocken in c

---
+++ C_Turorial/C Basics/Tokens in C/Table of Content/Operators/method1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>  
+int main() {
+    int a = 10, b = 5;  
+  
+    // '+' is an arithmetic operator used
+    // for addition
+    int sum = a + b;  
+    printf("%d", sum); 
+    return 0;
+}

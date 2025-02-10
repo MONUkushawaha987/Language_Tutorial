1cb5c914e92654059818e58b9b7d73dd1df77fc7
MONU KUSHAWAHA <monukushawaha7@gmail.com>
2025-02-10 11:19:59 +0530

    constant tocken in c

---
+++ C_Turorial/C Basics/Tokens in C/Table of Content/Constants/method1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>  
+int main() {
+  
+    // 'MAX_VALUE' is a constant that holds
+    // a fixed value
+    const int MAX_VALUE = 100;  
+    printf("%d", MAX_VALUE);
+    return 0;
+}

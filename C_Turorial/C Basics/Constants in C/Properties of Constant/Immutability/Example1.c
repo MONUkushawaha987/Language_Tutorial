f9f2bb4b9cf25f8815ddcb97745ae883f5c416cb
MONU KUSHAWAHA <monukushawaha7@gmail.com>
2025-02-10 11:29:26 +0530

    Immutability in c

---
+++ C_Turorial/C Basics/Constants in C/Properties of Constant/Immutability/Example1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+  
+    // Declaring a constant variable
+    const int a;
+  
+    // Initializing constant variable var after declaration
+    a = 20;
+    printf("%d", a);
+    return 0;
+}

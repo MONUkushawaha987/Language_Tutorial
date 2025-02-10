7f496c77935cd743e4499d44815436026e2e7177
MONU KUSHAWAHA <monukushawaha7@gmail.com>
2025-02-10 11:17:09 +0530

    keyword tocken in c

---
+++ C_Turorial/C Basics/Tokens in C/Table of Content/Keywords/method1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>  
+int main() {
+   
+    // 'int' is a keyword used to define
+    // variable type
+    int x = 5;  
+    printf("%d", x);
+  
+    // 'return' is a keyword used to exit
+    // main function
+    return 0;  
+}

fb1f2db4781aa82b408224431a985f4be3edf70f
MONU KUSHAWAHA <monukushawaha7@gmail.com>
2025-02-14 12:43:52 +0530

    Printing left aligned string of width 50

---
+++ C_Turorial/C InputOutput/Basic Input and Output in C/Standard Output Function – printf()/printf in C/leftAlign.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+  
+     char s[] = "Welcome to GfG!";
+  
+      // Printing left aligned string of width 50
+    printf("%-50s", s);
+      printf("Geeks");
+    return 0;
+}

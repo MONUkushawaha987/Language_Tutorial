e740ae19fe35a14e901a868babad3b4d72fc758a
MONU KUSHAWAHA <monukushawaha7@gmail.com>
2025-02-15 13:09:24 +0530

    The if and else keywords are used for conditional branching

---
+++ Java Tutorial/Basics of Java/Java Keyword/6. if and else Keywords/IfElseExample.java
@@ -0,0 +1,12 @@
+// Program to demonstrate 'if' and 'else' keywords
+class IfElseExample {
+    public static void main(String[] args) {
+        int age = 18;
+
+        if (age >= 18) {
+            System.out.println("You are an adult.");
+        } else {
+            System.out.println("You are a minor.");
+        }
+    }
+}

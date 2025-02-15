0515c8d4ebf1d183b0e23202544b30e5d1e224fd
MONU KUSHAWAHA <monukushawaha7@gmail.com>
2025-02-15 13:03:56 +0530

    The public keyword is an access modifier that makes a class, method, or variable accessible from anywhere

---
+++ Java Tutorial/Basics of Java/Java Keyword/2. public Keyword/PublicExample.java
@@ -0,0 +1,6 @@
+// Program to demonstrate the 'public' keyword
+public class PublicExample {
+    public static void main(String[] args) {
+        System.out.println("This is a public class and method.");
+    }
+}

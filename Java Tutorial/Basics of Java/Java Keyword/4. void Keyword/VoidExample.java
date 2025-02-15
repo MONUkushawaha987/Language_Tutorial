30e187937c522174e7235963ffe446e6c045992d
MONU KUSHAWAHA <monukushawaha7@gmail.com>
2025-02-15 13:06:40 +0530

    The void keyword is used to specify that a method does not return any value

---
+++ Java Tutorial/Basics of Java/Java Keyword/4. void Keyword/VoidExample.java
@@ -0,0 +1,10 @@
+// Program to demonstrate the 'void' keyword
+class VoidExample {
+    public static void main(String[] args) {
+        printMessage();
+    }
+
+    static void printMessage() { // Method with no return value
+        System.out.println("This is a void method.");
+    }
+}

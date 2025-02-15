57dd2a8e6ab65c54037a3c776fba8571b435d48c
MONU KUSHAWAHA <monukushawaha7@gmail.com>
2025-02-15 13:08:31 +0530

    The int keyword is used to declare an integer variable

---
+++ Java Tutorial/Basics of Java/Java Keyword/5. int Keyword/IntExample.java
@@ -0,0 +1,7 @@
+// Program to demonstrate the 'int' keyword
+class IntExample {
+    public static void main(String[] args) {
+        int number = 10;
+        System.out.println("Number: " + number);
+    }
+}

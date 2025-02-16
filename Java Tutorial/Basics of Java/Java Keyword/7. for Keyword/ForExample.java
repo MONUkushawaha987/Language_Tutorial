7962fb34d8b8cc6f44cd3923a3cb7d35c8d55ff7
MONU KUSHAWAHA <monukushawaha7@gmail.com>
2025-02-16 15:14:44 +0530

    The for keyword is used to create a loop that iterates a specific number of times

---
+++ Java Tutorial/Basics of Java/Java Keyword/7. for Keyword/ForExample.java
@@ -0,0 +1,10 @@
+// Program to demonstrate the 'for' keyword
+class ForExample {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Iteration: " + i);
+        }
+    }
+}
+
+

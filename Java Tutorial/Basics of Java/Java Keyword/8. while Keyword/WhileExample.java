407e826813a66b2169d6e510a5e0528224273275
MONU KUSHAWAHA <monukushawaha7@gmail.com>
2025-02-16 15:15:30 +0530

    The while keyword is used to create a loop that continues as long as a condition is true

---
+++ Java Tutorial/Basics of Java/Java Keyword/8. while Keyword/WhileExample.java
@@ -0,0 +1,12 @@
+// Program to demonstrate the 'while' keyword
+class WhileExample {
+    public static void main(String[] args) {
+        int i = 1;
+        while (i <= 5) {
+            System.out.println("Iteration: " + i);
+            i++;
+        }
+    }
+}
+
+

c232165defbfde872c4a3d49ee265742c6ee82f8
MONU KUSHAWAHA <monukushawaha7@gmail.com>
2025-02-16 15:16:29 +0530

    The return keyword is used to return a value from a method

---
+++ Java Tutorial/Basics of Java/Java Keyword/9. return Keyword/ReturnExample.java
@@ -0,0 +1,13 @@
+// Program to demonstrate the 'return' keyword
+class ReturnExample {
+    public static void main(String[] args) {
+        int result = add(5, 10);
+        System.out.println("Sum: " + result);
+    }
+
+    static int add(int a, int b) {
+        return a + b; // Returns the sum of a and b
+    }
+}
+// The return keyword is used to return a value from a method.
+

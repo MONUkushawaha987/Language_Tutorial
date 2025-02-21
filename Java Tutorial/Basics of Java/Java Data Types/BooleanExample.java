08359cc7342cc5fcb76a67b1dd366289400f98d0
MONU KUSHAWAHA <monukushawaha7@gmail.com>
2025-02-21 21:49:15 +0530

    Boolean data type example in java

---
+++ Java Tutorial/Basics of Java/Java Data Types/BooleanExample.java
@@ -0,0 +1,6 @@
+public class BooleanExample {
+    public static void main(String[] args) {
+        boolean myBoolean = true; // true or false
+        System.out.println("boolean value: " + myBoolean);
+    }
+}

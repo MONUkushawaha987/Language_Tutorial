a34d6a0873facaacc071ff2ebb27eec4545b55b3
MONU KUSHAWAHA <monukushawaha7@gmail.com>
2025-02-21 21:47:43 +0530

    float data type example in java

---
+++ Java Tutorial/Basics of Java/Java Data Types/FloatExample.java
@@ -0,0 +1,6 @@
+public class FloatExample {
+    public static void main(String[] args) {
+        float myFloat = 3.14f; // 32-bit floating point
+        System.out.println("float value: " + myFloat);
+    }
+}

9bfb5dcd94e5935ae90dd2103055f356dd6c955a
MONU KUSHAWAHA <monukushawaha7@gmail.com>
2025-02-21 21:48:16 +0530

    Double data type example in java

---
+++ Java Tutorial/Basics of Java/Java Data Types/DoubleExample.java
@@ -0,0 +1,6 @@
+public class DoubleExample {
+    public static void main(String[] args) {
+        double myDouble = 3.1415926535; // 64-bit floating point
+        System.out.println("double value: " + myDouble);
+    }
+}

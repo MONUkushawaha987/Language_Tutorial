0c08dcce02f6cc70137611b73610374757f60cef
MONU KUSHAWAHA <monukushawaha7@gmail.com>
2025-02-21 21:45:22 +0530

    Byte example in java

---
+++ Java Tutorial/Basics of Java/Java Data Types/ByteExample.java
@@ -0,0 +1,6 @@
+public class ByteExample {
+    public static void main(String[] args) {
+        byte myByte = 100; // 8-bit integer (-128 to 127)
+        System.out.println("byte value: " + myByte);
+    }
+}

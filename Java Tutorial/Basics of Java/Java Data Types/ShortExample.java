97b0af6d0f31c4ec969d987e93e2f455deeac904
MONU KUSHAWAHA <monukushawaha7@gmail.com>
2025-02-21 21:46:03 +0530

    Short example in java

---
+++ Java Tutorial/Basics of Java/Java Data Types/ShortExample.java
@@ -0,0 +1,6 @@
+public class ShortExample {
+    public static void main(String[] args) {
+        short myShort = 20000; // 16-bit integer (-32,768 to 32,767)
+        System.out.println("short value: " + myShort);
+    }
+}

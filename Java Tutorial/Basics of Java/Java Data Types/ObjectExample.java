4f818b14211290e624da36b8ecafeb7266a8b833
MONU KUSHAWAHA <monukushawaha7@gmail.com>
2025-02-21 21:53:13 +0530

    Object data type example in java

---
+++ Java Tutorial/Basics of Java/Java Data Types/ObjectExample.java
@@ -0,0 +1,6 @@
+public class ObjectExample {
+    public static void main(String[] args) {
+        Object myObject = "Hello, Java!"; // Object can hold any type
+        System.out.println("Object value: " + myObject);
+    }
+}

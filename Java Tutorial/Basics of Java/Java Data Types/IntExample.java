81249b4fb3cefbbdc64540f6f3d13caf8487f5df
MONU KUSHAWAHA <monukushawaha7@gmail.com>
2025-02-21 21:46:35 +0530

    Int example in java

---
+++ Java Tutorial/Basics of Java/Java Data Types/IntExample.java
@@ -0,0 +1,6 @@
+public class IntExample {
+    public static void main(String[] args) {
+        int myInt = 100000; // 32-bit integer (-2^31 to 2^31-1)
+        System.out.println("int value: " + myInt);
+    }
+}

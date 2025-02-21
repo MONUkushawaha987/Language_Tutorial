e4dabc5b4d985b36b9ebd44d3cdc6f4da8f46076
MONU KUSHAWAHA <monukushawaha7@gmail.com>
2025-02-21 21:48:46 +0530

    Char data type example in java

---
+++ Java Tutorial/Basics of Java/Java Data Types/CharExample.java
@@ -0,0 +1,6 @@
+public class CharExample {
+    public static void main(String[] args) {
+        char myChar = 'A'; // 16-bit Unicode character
+        System.out.println("char value: " + myChar);
+    }
+}

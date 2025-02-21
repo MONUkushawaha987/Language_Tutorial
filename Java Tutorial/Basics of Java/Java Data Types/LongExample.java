cdc8aae4028e6cfec82fed1e10ad1c1635b9af20
MONU KUSHAWAHA <monukushawaha7@gmail.com>
2025-02-21 21:47:05 +0530

    long data type example in java

---
+++ Java Tutorial/Basics of Java/Java Data Types/LongExample.java
@@ -0,0 +1,6 @@
+public class LongExample {
+    public static void main(String[] args) {
+        long myLong = 10000000000L; // 64-bit integer (-2^63 to 2^63-1)
+        System.out.println("long value: " + myLong);
+    }
+}

191a8a43262696f03069908609c0a48c85b7da7f
MONU KUSHAWAHA <monukushawaha7@gmail.com>
2025-02-21 21:50:31 +0530

    Array data type example in java

---
+++ Java Tutorial/Basics of Java/Java Data Types/ArrayExample.java
@@ -0,0 +1,9 @@
+public class ArrayExample {
+    public static void main(String[] args) {
+        int[] myIntArray = {1, 2, 3, 4, 5}; // Array of integers
+        System.out.print("int[] Array: ");
+        for (int num : myIntArray) {
+            System.out.print(num + " ");
+        }
+    }
+}

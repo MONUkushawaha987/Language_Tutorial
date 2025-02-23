550fb5e21963a257744da5fb0419b0612566bd31
MONU KUSHAWAHA <monukushawaha7@gmail.com>
2025-02-23 23:54:26 +0530

    ternary operator in java

---
+++ Java Tutorial/Basics of Java/Java Operators/TernaryOperator.java
@@ -0,0 +1,7 @@
+public class TernaryOperator {
+    public static void main(String[] args) {
+        int a = 10, b = 5;
+        int result = (a > b) ? a : b; // If a > b, result = a, else result = b
+        System.out.println("Result: " + result); // 10
+    }
+}

03ac8862d6890b8efed7f78652ea746f9c0217dc
MONU KUSHAWAHA <monukushawaha7@gmail.com>
2025-02-25 21:25:34 +0530

    while loop example in java

---
+++ Java Tutorial/Basics of Java/Java Loops/WhileLoopExample.java
@@ -0,0 +1,11 @@
+public class WhileLoopExample {
+    public static void main(String[] args) {
+        int i = 1;
+
+        // Print numbers from 1 to 5
+        while (i <= 5) {
+            System.out.println("Number: " + i);
+            i++;
+        }
+    }
+}

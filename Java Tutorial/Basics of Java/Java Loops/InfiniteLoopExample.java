89b1d5d7308aaac4e60e5a370e37296e3229053b
MONU KUSHAWAHA <monukushawaha7@gmail.com>
2025-02-25 21:28:17 +0530

    Infinite loop example in java

---
+++ Java Tutorial/Basics of Java/Java Loops/InfiniteLoopExample.java
@@ -0,0 +1,8 @@
+public class InfiniteLoopExample {
+    public static void main(String[] args) {
+        // Infinite loop
+        while (true) {
+            System.out.println("This will run forever!");
+        }
+    }
+}

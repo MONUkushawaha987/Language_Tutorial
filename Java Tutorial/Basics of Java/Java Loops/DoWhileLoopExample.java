92e2e4e4389d97cccce5f79fce61b22843e80441
MONU KUSHAWAHA <monukushawaha7@gmail.com>
2025-02-25 21:26:07 +0530

    do while loop example in java

---
+++ Java Tutorial/Basics of Java/Java Loops/DoWhileLoopExample.java
@@ -0,0 +1,11 @@
+public class DoWhileLoopExample {
+    public static void main(String[] args) {
+        int i = 1;
+
+        // Print numbers from 1 to 5
+        do {
+            System.out.println("Number: " + i);
+            i++;
+        } while (i <= 5);
+    }
+}

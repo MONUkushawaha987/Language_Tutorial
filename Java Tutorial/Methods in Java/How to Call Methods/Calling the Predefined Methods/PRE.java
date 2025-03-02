4e5819606a970e52b2527084971f0bce0ee86127
MONU KUSHAWAHA <monukushawaha7@gmail.com>
2025-03-02 23:43:52 +0530

    Java Program to call Predefined Method

---
+++ Java Tutorial/Methods in Java/How to Call Methods/Calling the Predefined Methods/PRE.java
@@ -0,0 +1,13 @@
+// Java Program to call Predefined Methods
+public class PRE {
+
+    public static void main(String[] args) {
+      
+        // Creating object of the class in
+        // main() method
+        PRE ob = new PRE();
+
+        // Calling predefined method
+        System.out.println(ob.hashCode());
+    }
+}

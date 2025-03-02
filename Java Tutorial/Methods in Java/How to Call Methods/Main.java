9451dca2ecf232d09fd0cd6a7935a9f89cc5d03a
MONU KUSHAWAHA <monukushawaha7@gmail.com>
2025-03-02 23:36:00 +0530

    Method calling in java

---
+++ Java Tutorial/Methods in Java/How to Call Methods/Main.java
@@ -0,0 +1,15 @@
+class Main {
+  
+    // User-defined method
+    void hello() {
+        System.out.println("This is a user-defined method.");
+    }
+    public static void main(String[] args) {
+      
+        // Create an object
+        Main obj = new Main(); 
+      
+        // Call the method
+        obj.hello();          
+    }
+}

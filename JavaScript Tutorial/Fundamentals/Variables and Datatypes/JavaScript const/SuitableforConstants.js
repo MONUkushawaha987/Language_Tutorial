be6b8603e487e1e442271722f5a6ac2d219d5f87
Monu Kushawaha <91782501+MONUkushawaha987@users.noreply.github.com>
2025-03-27 23:42:27 +0530

    The const keyword is ideal for defining constants or variables whose values should not change during the program’s execution

---
+++ JavaScript Tutorial/Fundamentals/Variables and Datatypes/JavaScript const/SuitableforConstants.js
@@ -0,0 +1,3 @@
+const PI = 3.14159;
+const MAX_USERS = 100;
+PI=10
--- JavaScript Tutorial/Fundamentals/Variables and Datatypes/JavaScript const/noRedaclaration.js
+++ JavaScript Tutorial/Fundamentals/Variables and Datatypes/JavaScript const/noRedaclaration.js
@@ -1,2 +1,3 @@
 // const a = 10;   I have commeted because of error returning
 const a = 20; // SyntaxError: Identifier 'a' has already been declared
+
